d501d1ca64c89441d4dc2fd3c33eba45e8497cc1
Fahreza17 <indminecraft52@gmail.com>
2023-11-04 20:55:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,173 @@
+import React, { createContext, useContext, useReducer, useEffect } from 'react';
+import './App.css';
 
-import * as React from 'react';
+const initialState = {
+  squares: Array(9).fill(null),
+  nextValue: 'X',
+  winner: null,
+};
+
+const GameContext = createContext();
+
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case 'SELECT_SQUARE':
+      if (state.squares[action.square] || state.winner) {
+        return state;
+      }
+      const newSquares = state.squares.slice();
+      newSquares[action.square] = state.nextValue;
+      const newWinner = calculateWinner(newSquares);
+      return {
+        ...state,
+        squares: newSquares,
+        nextValue: state.nextValue === 'X' ? 'O' : 'X',
+        winner: newWinner,
+      };
+    case 'DRAW':
+      return { ...state, winner: "Cat's game" };
+    case 'RESTART':
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+
+function useGame() {
+  return useContext(GameContext);
+}
+
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+  const { state, dispatch } = useGame();
 
+  function selectSquare(square) {
+    dispatch({ type: 'SELECT_SQUARE', square });
   }
 
   function restart() {
+    dispatch({ type: 'RESTART' });
   }
 
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
+      <button
+        className="square bg-blue-200 hover:bg-blue-400 text-3xl font-bold p-2 rounded h-16"
+        onClick={() => selectSquare(i)}
+      >
+        {state.squares[i]}
       </button>
     );
   }
 
   return (
-    <div>
-      <div >STATUS</div>
-      <div >
+    <div className="text-center mt-4">
+      <div className="text-2xl font-bold mb-2">
+        STATUS: {state.winner ? `Winner: ${state.winner}` : `Next player: ${state.nextValue}`}
+      </div>
+      <div className="grid grid-cols-3 gap-1">
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-      </div>
-      <div >
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-      </div>
-      <div >
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button onClick={restart}>
-        restart
+      <button
+        className="mt-4 bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+        onClick={restart}
+      >
+        Restart
       </button>
     </div>
   );
 }
 
+function findBestMove(squares, maximizingPlayer) {
+  const availableSquares = squares
+    .map((value, index) => (value ? null : index))
+    .filter((square) => square !== null);
+
+  if (calculateWinner(squares)) {
+    return { score: maximizingPlayer ? -1 : 1 };
+  } else if (availableSquares.length === 0) {
+    return { score: 0 };
+  }
+
+  const moves = [];
+  for (let i = 0; i < availableSquares.length; i++) {
+    const move = {};
+    move.index = availableSquares[i];
+    squares[availableSquares[i]] = maximizingPlayer ? 'O' : 'X';
+
+    const result = findBestMove(squares, !maximizingPlayer);
+    move.score = result.score;
+
+    squares[availableSquares[i]] = null;
+    moves.push(move);
+  }
+
+  let bestMove;
+  if (maximizingPlayer) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
+}
+
+
 function Game() {
+  const { state, dispatch } = useGame();
+
+  useEffect(() => {
+    if (state.nextValue === 'O' && !state.winner) {
+      const emptySquares = state.squares.map((value, index) => (value ? null : index));
+      const availableSquares = emptySquares.filter((square) => square !== null);
+
+      if (availableSquares.length > 0) {
+        const bestMove = findBestMove(state.squares.slice(), true);
+        setTimeout(() => {
+          dispatch({ type: 'SELECT_SQUARE', square: bestMove.index });
+        }, 500);
+      }
+    }
+  }, [state.nextValue, state.squares, state.winner, dispatch]);
+
+  
+  useEffect(() => {
+    if (!state.winner && state.squares.every(Boolean)) {
+      
+      dispatch({ type: 'DRAW' });
+    }
+  }, [state.squares, state.winner, dispatch]);
+
   return (
-    <div >
-      <div >
+    <div className="w-full max-w-md mx-auto">
+      <div className="bg-white rounded shadow p-4">
         <Board />
       </div>
     </div>
   );
 }
 
-// eslint-disable-next-line no-unused-vars
-function calculateStatus(winner, squares, nextValue) {
-  return winner
-    ? `Winner: ${winner}`
-    : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
-}
-
-// eslint-disable-next-line no-unused-vars
-function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
-}
-
-// eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -88,8 +188,17 @@ function calculateWinner(squares) {
   return null;
 }
 
+
 function App() {
-  return <Game />;
+  const [state, dispatch] = useReducer(gameReducer, initialState);
+
+  return (
+    <GameContext.Provider value={{ state, dispatch }}>
+      <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+        <Game />
+      </div>
+    </GameContext.Provider>
+  );
 }
 
 export default App;
